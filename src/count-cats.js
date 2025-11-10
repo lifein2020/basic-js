d670248941bcad447065e090631867ab9ec711d4
Svetlana Zh <lady_zhivtsova@gmail.com>
2025-11-11 01:02:03 +0200

    feat: count-cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,9 +14,18 @@ const { NotImplementedError } = require('../lib');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+function countCats(matrix) {
+  let cats = 0;
+
+  matrix.forEach((item) => {
+    item.forEach(() => {
+      if (item[i] === '^^') {
+        cats ++;
+      }
+    })
+  });
+  
+  return result;
 }
 
 module.exports = {
